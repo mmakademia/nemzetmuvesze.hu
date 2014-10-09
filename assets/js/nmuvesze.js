9ac874b0089d8faee69261c2139687720455d5ed
calvez <nemeth.lorant@gmail.com>
2014-10-09 15:29:42 +0200

    toggle button fixed

--- assets/js/nmuvesze.js
+++ assets/js/nmuvesze.js
@@ -8,19 +8,23 @@ var section_show_time = 1000;
 // jQuery stuff
 jQuery(document).ready(function($) {
 
-	// Switch section
-	$("a", '.navbar').click(function() 
-	{
-		if( ! $(this).hasClass('active') ) { 
-			current_item = this;
-			// close all visible divs with the class of .section
-			$('.section:visible').fadeOut( section_hide_time, function() { 
-				$('a', '.navbar').removeClass( 'active' );  
-				$(current_item).addClass( 'active' );
-				var new_section = $( $(current_item).attr('href') );
-				new_section.fadeIn( section_show_time );
-			} );
-		}
-		return false;
-	});		
+    // Switch section
+    $("a", '.navbar-collapse').click(function() {
+        if (!$(this).hasClass('active')) {
+            current_item = this;
+            // close all visible divs with the class of .section
+            $('.section:visible').fadeOut(section_hide_time, function() {
+                $('a', '.navbar-collapse').removeClass('active');
+                $(current_item).addClass('active');
+                var new_section = $($(current_item).attr('href'));
+                new_section.fadeIn(section_show_time);
+            });
+        }
+        return false;
+    });
+
+    // auto toggle navbar - fuck it!
+    $('.nav a').on('click', function() {
+        $(".navbar-toggle").click()
+    });
 });
